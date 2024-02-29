f2efdb7451de948a1cb67300d3dd27412f70f006
Raphael Palmer <palmer@Raphaels-Mac-Studio.local>
2024-02-28 22:16:25 -0300

    bandcamp api

--- dengue-dashboards/src/App/DataAnalysis/APIFetch.ts
+++ dengue-dashboards/src/App/DataAnalysis/APIFetch.ts
@@ -1,4 +1,4 @@
-export const bandcampWebApiURL = ''
+export const bandcampWebApiURL = 'https://bandcamp.com/api/sales/1/sales_report'
 export const spotifyWebApiURL = ''
 export const soundcloudWebApiURL = ''
 export const dataService = 'http://localhost:3001/api/data-service'
